2006d585c771f1aeb495df4e8594323b6631faeb
Manas Kumar Biswal <134088923+manasku123@users.noreply.github.com>
2024-04-11 12:41:18 +0530

    Add files via upload

---
+++ project/Controllers/EmployeeDataController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeDataController : ControllerBase
+    {
+        public EmployeeDataController()
+        {
+            
+        }
+        private readonly IEmployeeBusiness _EmployeeBusiness;
+        public EmployeeDataController(IEmployeeBusiness EmployeeBusiness)
+        {
+            _EmployeeBusiness = EmployeeBusiness;
+        }
+
+        [HttpGet()]
+        public IEnumerable<EmployeeS> Getemployee()
+        {
+            var result = _EmployeeBusiness.GetEmployee();
+            return result;
+        }
+    }
+}
+      
+    
+
---
+++ project/Employee.Business/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Employee.Business.Business
+{
+    public class EmployeeBusiness : IEmployeeBusiness
+    {
+        private List<EmployeeS> _employee;
+        public EmployeeBusiness()
+        {
+            List<EmployeeS> employee = new()
+            {
+                new(){EmployeeId=1,Name="raghav",Position="JrD",Salary=40000,JoinDate=new DateTime(2024, 05, 10)},
+                new(){EmployeeId=2,Name="aryan",Position="SrD",Salary=50000,JoinDate=new DateTime(2024, 05, 15)},
+                new(){EmployeeId=3,Name="gita",Position="Sale",Salary=30000,JoinDate=new DateTime(2024, 05, 20)},
+                new(){EmployeeId=4,Name="raaz",Position="SrD",Salary=41000,JoinDate=new DateTime(2024, 05, 05)},
+             
+            };
+            _employee = employee;
+        }
+
+        public IEnumerable<EmployeeS> GetEmployee()
+        {
+            var result = _employee;
+            return result;
+        }
+
+    }
+}
